93dc00e55f78ddbdae9792fd37832501e53bf012
AyeshkantJena <jenaayeshkant@gmail.com>
2025-06-29 00:46:53 +0530

    Added solutions for Week 2

---
+++ Week2/CustomerCommLib/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NUnit.Framework;
+using CustomerCommLib; // Reference to your main project
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm; // Fully qualify the type to avoid ambiguity
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);  // Simulate successful email sending
+
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object); // Fully qualify the type
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            var result = _customerComm.SendMailToCustomer();
+            Assert.That(result, Is.True);
+        }
+    }
+}
